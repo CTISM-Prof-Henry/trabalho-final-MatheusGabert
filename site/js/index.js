7cf6584fcd99da2d7a9767481cfbcf1afb95dbf4
Matheus Gabert Pereira da Silva <69723708+MatheusGabert@users.noreply.github.com>
2026-06-08 13:10:04 -0300

    Add files via upload

---
+++ site/js/index.js
@@ -0,0 +1,25 @@
+function filterElements(genero, botao) {
+
+    const elementos = document.querySelectorAll(".div_gamescard");
+
+    elementos.forEach((elemento) => {
+        elemento.classList.remove("show");
+
+        if (genero === "all" || elemento.classList.contains(genero)) {
+            elemento.classList.add("show");
+        }
+    });
+
+    const botoes = document.querySelectorAll(".button");
+
+    botoes.forEach((botaoAtual) => {
+        botaoAtual.classList.remove("item_ativo");
+    });
+
+    botao.classList.add("item_ativo");
+}
+
+window.onload = function() {
+    const homeButton = document.querySelector(".item_ativo");
+    filterElements("all", homeButton);
+}
